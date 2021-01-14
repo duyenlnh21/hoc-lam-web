ca6f661f8991b11c57e00d8350b7a9417fdc8632
lethiduyen000097 <duyenlt060097@gmail.com>
2021-01-14 15:55:33 +0700

    update Fragments

--- chapter/src/WithFragments.js
+++ chapter/src/WithFragments.js
@@ -4,8 +4,16 @@ class WithFragments extends Component {
     render() {
         return (
             <Fragment>
-                <h1>With Fragments 1</h1>
-                <p>Doesn't have any unused DOM elements.</p>
+                <h1>Hướng dẫn làm mứt me ăn tết</h1>
+                <h3>Chuẩn bị nguyên liệu</h3>
+                <p> Me xanh ( 1 kg); Đường (0,3 kg); Muối (2 thìa) </p>
+                <h3>Các bước thực hiện</h3>
+                <h4>Bước 1:</h4>
+                <p>Bóc vỏ, bỏ hột, rửa sạch, ngâm nước muối</p>
+                <h4>Bước 2:</h4>
+                <p>Ngâm đường</p>
+                <h4>Bước 3:</h4>
+                <p>Phơi khô</p>
             </Fragment>
         );
     }
--- chapter/src/WithoutFragments.js
+++ chapter/src/WithoutFragments.js
@@ -4,12 +4,17 @@ class WithoutFragments extends Component {
     render() {
         return (
             <div>
-                <h1>Without Fragments1</h1>
-                <p>
-                    Adds an extra <code>div</code> element.
-                </p>
+                <h1>Hướng dẫn làm mứt me ăn tết</h1>
+                <h3>Chuẩn bị nguyên liệu</h3>
+                <p> Me xanh ( 1 kg); Đường (0,3 kg); Muối (2 thìa) </p>
+                <h3>Các bước thực hiện</h3>
+                <h4>Bước 1:</h4>
+                <p>Bóc vỏ, bỏ hột, rửa sạch, ngâm nước muối</p>
+                <h4>Bước 2:</h4>
+                <p>Ngâm đường</p>
+                <h4>Bước 3:</h4>
+                <p>Phơi khô</p>
             </div>
-            
         );
     }
 }
